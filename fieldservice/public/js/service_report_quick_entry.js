94306b79453478dd5bbe63b39030f0c1b9d8133b
Beate Trenziok <dev@itsdave.de>
2024-08-01 08:50:20 +0200

    Anpassung QE SR

--- fieldservice/public/js/service_report_quick_entry.js
+++ fieldservice/public/js/service_report_quick_entry.js
@@ -1,34 +1,30 @@
 frappe.provide('frappe.ui.form');
 
-frappe.ui.form.ServiceReportQuickEntryForm = frappe.ui.form.QuickEntryForm.extend({
-    init: function(doctype, after_insert, init_callback, doc, force) {
-        this._super(doctype, after_insert, init_callback, doc, force);
+frappe.ui.form.ServiceReportQuickEntryForm = class ServiceReportQuickEntryForm extends frappe.ui.form.QuickEntryForm {
+    constructor(doctype, after_insert, init_callback, doc, force) {
+        super(doctype, after_insert, init_callback, doc, force);
         this.skip_redirect_on_error = true;
-    },
+    }
 
-    render_dialog: function() {
-		//this.mandatory = this.mandatory.concat(this.get_variant_fields());
-		this._super();
-		console.log("hi")
-		let me = this
-		console.log(me)
-        console.log(me.dialog.fields_dict)
-        // Überprüfen, ob das customer-Feld in fields_dict vorhanden und definiert ist
+    render_dialog() {
+        super.render_dialog();
+
+        let me = this;
+        console.log("Dialog fields_dict:", me.dialog.fields_dict);
+
+        // Ensure customer field is handled properly
         if (me.dialog.fields_dict && me.dialog.fields_dict.customer) {
             me.dialog.fields_dict.customer.get_query = function() {
                 return {
                     filters: {
-                        'disabled': 0
+                        disabled: 0
                     }
                 };
             };
         } else {
             console.error("Customer field not found or undefined in fields_dict.");
-
-
-         }
+        }
     }
-});
-
+};
 
 
